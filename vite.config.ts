5f5aacc39cfaf5cf2a57a4945c08d695809bc8d6
Prascyber <pg112233acc@gmail.com>
2025-11-01 14:38:16 +0000

    fix: added base path for GitHub Pages and redirect file

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "development" ? "/" : "/debt-conqueror/", // 👈 set your repo name here
+
   server: {
     host: "::",
     port: 8080,
